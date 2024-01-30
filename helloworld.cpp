9e8654ac83be5fd876eedd6b2cbce339413eff19
Daniela <19047@portalcedi.edu.mx>
2024-01-30 13:44:16 -0600

    cambioedad

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,15 @@
 # include <iostream>
 using namespace std;
 int main(){
+    int edad;
+
+    cout<<"escribe tu edad"<<endl;
+    cin>>edad;
+    if (edad>18){
+cout<<"eres mayor de edad"<<endl;
+    }else{
+        cout<<"eres menor de edad"<<endl;
+    }
 return 0;
 
 }
